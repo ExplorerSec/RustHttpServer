24f66cb08164c9d2bdcb64f6812f551d28dea23c
ExplorerSec <gaoweixu@foxmail.com>
2025-05-15 09:25:23 +0800

    0.2.0 线程池服务器

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,25 +1,35 @@
+use rust_http_server::ThreadPool;
 use std::{fs, io::{Read, Write}, net::{TcpListener, TcpStream}, thread, time::Duration};
 fn main() {
     let addr ="127.0.0.1:17788";
+    let pool =ThreadPool::new(4);
+
     let listener = TcpListener::bind(addr).unwrap();
-    for stream in listener.incoming(){
+    println!("Running on {addr}");
+    
+
+    for stream in listener.incoming(){  // 测试-仅处理两个请求: .take(2){
         let stream = stream.unwrap();
         
-        thread::spawn(||{
+        pool.execute(||{
             handle_stream(stream)
         });
     }
+
+    println!("[-] MainFunc Shutting down.");
 }
 
 fn handle_stream(mut stream: TcpStream){
     let mut buffer = [0;1024];
     stream.read(&mut buffer).unwrap();
-    println!("{}",String::from_utf8_lossy(&buffer));
+    // println!("{}",String::from_utf8_lossy(&buffer));
 
     // normal get
     let get =b"GET / HTTP/1.1";
     // slow get
     let get_slow =b"GET /slow HTTP/1.1";
+    // cookie test
+    let get_cookie = b"GET /cookie HTTP/1.1";
 
     let (status_line,filename) = if buffer.starts_with(get){
         ("HTTP/1.1 200 OK \r\n\r\n","./html/index.html")
@@ -27,6 +37,8 @@ fn handle_stream(mut stream: TcpStream){
         // slow get (use sleep)
         thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK \r\n\r\n","./html/index.html")
+    }else if buffer.starts_with(get_cookie){
+        ("HTTP/1.1 200 OK\r\nContent-type: text/html\r\nSet-Cookie: Expzero=Exp0\r\n\r\n","./html/index.html")
     }
     else{
         ("HTTP/1.1 404 NOT FOUND \r\n\r\n","./html/404.html")
