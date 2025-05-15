8d13e755daeaa07ebb06c658ade5041b123b44df
ExplorerSec <gaoweixu@foxmail.com>
2025-05-15 10:23:17 +0800

    0.2.1 code format

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,51 +1,52 @@
 use rust_http_server::ThreadPool;
-use std::{fs, io::{Read, Write}, net::{TcpListener, TcpStream}, thread, time::Duration};
+use std::{
+    fs,
+    io::prelude::*,
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
 fn main() {
-    let addr ="127.0.0.1:17788";
-    let pool =ThreadPool::new(4);
+    let addr = "127.0.0.1:17788";
+    let pool = ThreadPool::new(4);
 
     let listener = TcpListener::bind(addr).unwrap();
     println!("Running on {addr}");
-    
 
-    for stream in listener.incoming(){  // 测试-仅处理两个请求: .take(2){
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
-        
-        pool.execute(||{
-            handle_stream(stream)
-        });
+        pool.execute(|| handle_stream(stream));
     }
 
     println!("[-] MainFunc Shutting down.");
 }
 
-fn handle_stream(mut stream: TcpStream){
-    let mut buffer = [0;1024];
+fn handle_stream(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    // println!("{}",String::from_utf8_lossy(&buffer));
+    println!("\n{}\n", String::from_utf8_lossy(&buffer));
 
-    // normal get
-    let get =b"GET / HTTP/1.1";
-    // slow get
-    let get_slow =b"GET /slow HTTP/1.1";
-    // cookie test
-    let get_cookie = b"GET /cookie HTTP/1.1";
-
-    let (status_line,filename) = if buffer.starts_with(get){
-        ("HTTP/1.1 200 OK \r\n\r\n","./html/index.html")
-    }else if buffer.starts_with(get_slow){
-        // slow get (use sleep)
-        thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK \r\n\r\n","./html/index.html")
-    }else if buffer.starts_with(get_cookie){
-        ("HTTP/1.1 200 OK\r\nContent-type: text/html\r\nSet-Cookie: Expzero=Exp0\r\n\r\n","./html/index.html")
+    if let Ok(str) = std::str::from_utf8(&buffer) {
+        if let Some((start_line, _)) = str.split_once('\r') {
+            let (status_line, filename) = match start_line {
+                // normal get
+                "GET / HTTP/1.1" => ("HTTP/1.1 200 OK \r\n\r\n", "./html/index.html"),
+                // slow get
+                "GET /slow HTTP/1.1" => {
+                    thread::sleep(Duration::from_secs(5));
+                    ("HTTP/1.1 200 OK \r\n\r\n", "./html/index.html")
+                }
+                // cookie get
+                "GET /cookie HTTP/1.1" => (
+                    "HTTP/1.1 200 OK\r\nContent-type: text/html\r\nSet-Cookie: Expzero=Exp0\r\n\r\n",
+                    "./html/index.html",
+                ),
+                _ => ("HTTP/1.1 404 NOT FOUND \r\n\r\n", "./html/404.html"),
+            };
+            let content = fs::read_to_string(filename).unwrap();
+            let response = format!("{}{}", status_line, content);
+            stream.write(&response.as_bytes()).unwrap();
+            stream.flush().unwrap();
+        }
     }
-    else{
-        ("HTTP/1.1 404 NOT FOUND \r\n\r\n","./html/404.html")
-    };
-
-    let content =fs::read_to_string(filename).unwrap();
-    let response = format!("{}{}",status_line,content);
-    stream.write(&response.as_bytes()).unwrap();
-    stream.flush().unwrap();
 }
