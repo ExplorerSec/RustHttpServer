3fb29f7a14bd9375e9f0459636e01a1fe79374cc
ExplorerSec <gaoweixu@foxmail.com>
2025-05-17 14:53:19 +0800

    0.2.2 简单登录测试

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -27,7 +27,7 @@ fn handle_stream(mut stream: TcpStream) {
     println!("\n{}\n", String::from_utf8_lossy(&buffer));
 
     if let Ok(str) = std::str::from_utf8(&buffer) {
-        if let Some((start_line, _)) = str.split_once('\r') {
+        if let Some((start_line, content)) = str.split_once('\r') {
             let (status_line, filename) = match start_line {
                 // normal get
                 "GET / HTTP/1.1" => ("HTTP/1.1 200 OK \r\n\r\n", "./html/index.html"),
@@ -41,9 +41,23 @@ fn handle_stream(mut stream: TcpStream) {
                     "HTTP/1.1 200 OK\r\nContent-type: text/html\r\nSet-Cookie: Expzero=Exp0\r\n\r\n",
                     "./html/index.html",
                 ),
+                "GET /login HTTP/1.1" => ("HTTP/1.1 200 OK \r\n\r\n", "./html/login.html"),
+                "POST /login HTTP/1.1" => ("HTTP/1.1 200 OK \r\nSet-Cookie: Login=OK\r\n\r\n", ""),
+                "GET /user HTTP/1.1" => {
+                    if content.find("Login=OK").is_some() {
+                        ("HTTP/1.1 200 OK \r\n\r\n", "./html/user.html")
+                    } else {
+                        ("HTTP/1.1 404 NOT FOUND \r\n\r\n", "./html/404.html")
+                    }
+                }
                 _ => ("HTTP/1.1 404 NOT FOUND \r\n\r\n", "./html/404.html"),
             };
-            let content = fs::read_to_string(filename).unwrap();
+
+            let content = if !filename.is_empty() {
+                fs::read_to_string(filename).unwrap()
+            } else {
+                "".to_string()
+            };
             let response = format!("{}{}", status_line, content);
             stream.write(&response.as_bytes()).unwrap();
             stream.flush().unwrap();
